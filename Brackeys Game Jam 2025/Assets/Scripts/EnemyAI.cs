10e4122f32f3fb0e4f53e52b7b2d63ec2d18347b
WaffleIII <shacharherscu7@gmail.com>
2025-08-28 18:21:27 -0400

    Added more to enemy AI; gravity & collider

--- Brackeys Game Jam 2025/Assets/Scripts/EnemyAI.cs
+++ Brackeys Game Jam 2025/Assets/Scripts/EnemyAI.cs
@@ -5,6 +5,7 @@ public class EnemyAI : MonoBehaviour
 
     public GameObject player;
     public float speed;
+    public float distanceBetween;
 
     private float distance;
 
@@ -18,8 +19,12 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         distance = Vector2.Distance(transform.position, player.transform.position); // Gets the distance between the first point (tf.position) and the second point (player.tf.position)
-        Vector2 direction = player.transform.position - transform.position;
+        Vector2 direction = player.transform.position - transform.position; // Finds the position between the player and the enemy and uses the value as a direction (-1 to 1)
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        if(distance < distanceBetween)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        }
+
     }
 }
